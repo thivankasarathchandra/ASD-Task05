78774c0cd9d3674f4434a89b454a71513f0fee94
MadMan <thivanka97.bc@gmail.com>
2023-07-03 06:18:44 +0530

    Experiment 13 & 14

--- functional-programming/src/experiments/Experiment13.java
+++ functional-programming/src/experiments/Experiment13.java
@@ -8,28 +8,85 @@ import java.util.function.Supplier;
  * @author Dr Kevan Buckley, University of Wolverhampton, 2019
  ******************************************************************************/
 
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
 public class Experiment13 {
-  class NameSupplier implements Supplier<String>{
-    String[] names = { "Kevan", "John", "Matthew" };
-    int nextIndex = 0;
-    public String get() {
-      if(nextIndex< names.length) {
-        return names[nextIndex++];
-      }
-      return null;
-    }  
-  }
-  
-  public void run() {
-    NameSupplier ns = new NameSupplier();
+    class ProductSupplier implements Supplier<Product> {
+        List<Product> products;
+        int nextIndex = 0;
 
-    System.out.println(ns.get());
-    System.out.println(ns.get());
-    System.out.println(ns.get());
-    System.out.println(ns.get());
-  }
+        public ProductSupplier(String csvFile) {
+            Path pathToFile = Paths.get(csvFile);
+            try (Stream<String> lines = Files.lines(pathToFile)) {
+                products = lines
+                        .skip(1) // Skip the header line
+                        .map(Experiment13::lineToProduct) // Map each line to a Product object
+                        .collect(Collectors.toList()); // Collect results to a List
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-  public static void main(String[] args) {
-    new Experiment13().run();
-  }
+        public Product get() {
+            if(nextIndex < products.size()) {
+                return products.get(nextIndex++);
+            }
+            return null;
+        }
+    }
+
+    public void run() {
+        ProductSupplier ps = new ProductSupplier("products.csv");
+
+        System.out.println(ps.get());
+        System.out.println(ps.get());
+        System.out.println(ps.get());
+        System.out.println(ps.get());
+    }
+
+    private static Product lineToProduct(String line) {
+        String[] attributes = line.split(",");
+        int productID = Integer.parseInt(attributes[0]);
+        String productName = attributes[1];
+        String category = attributes[2];
+        double price = Double.parseDouble(attributes[3]);
+        int quantitySold = Integer.parseInt(attributes[4]);
+
+        return new Product(productID, productName, category, price, quantitySold);
+    }
+
+    public static void main(String[] args) {
+        new Experiment13().run();
+    }
 }
+
+
+//public class Experiment13 {
+//  class NameSupplier implements Supplier<String>{
+//    String[] names = { "Kevan", "John", "Matthew" };
+//    int nextIndex = 0;
+//    public String get() {
+//      if(nextIndex< names.length) {
+//        return names[nextIndex++];
+//      }
+//      return null;
+//    }  
+//  }
+//  
+//  public void run() {
+//    NameSupplier ns = new NameSupplier();
+//
+//    System.out.println(ns.get());
+//    System.out.println(ns.get());
+//    System.out.println(ns.get());
+//    System.out.println(ns.get());
+//  }
+//
+//  public static void main(String[] args) {
+//    new Experiment13().run();
+//  }
+//}
--- functional-programming/src/experiments/Experiment14.java
+++ functional-programming/src/experiments/Experiment14.java
@@ -8,24 +8,76 @@ import java.util.stream.Stream;
  * @author Dr Kevan Buckley, University of Wolverhampton, 2019
  ******************************************************************************/
 
-public class Experiment14 {
-  class NameStreamBuilder {
-    public Stream<String> build(){
-      Stream.Builder<String> builder = Stream.builder(); 
-      builder.add("Kevan");
-      builder.add("John");
-      builder.add("Matthew");
-      return builder.build();
-    }
-  }
-  
-  public void run() {
-    NameStreamBuilder builder = new NameStreamBuilder();
-    Stream <String> nameStream = builder.build();
-    nameStream.forEach(n -> System.out.println(n));
-  }
 
-  public static void main(String[] args) {
-    new Experiment14().run();
-  }
+import java.nio.file.*;
+import java.io.*;
+import java.util.stream.*;
+
+public class Experiment14 {
+    class ProductStreamBuilder {
+        String csvFile;
+
+        public ProductStreamBuilder(String csvFile) {
+            this.csvFile = csvFile;
+        }
+
+        public Stream<Product> build() {
+            Stream.Builder<Product> builder = Stream.builder();
+            Path pathToFile = Paths.get(csvFile);
+
+            try (Stream<String> lines = Files.lines(pathToFile)) {
+                lines
+                    .skip(1) // Skip the header line
+                    .map(Experiment14::lineToProduct) // Map each line to a Product object
+                    .forEach(builder::add); // Add each product to the builder
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+
+            return builder.build();
+        }
+    }
+
+    public void run() {
+        ProductStreamBuilder builder = new ProductStreamBuilder("products.csv");
+        Stream<Product> productStream = builder.build();
+        productStream.forEach(product -> System.out.println(product));
+    }
+
+    private static Product lineToProduct(String line) {
+        String[] attributes = line.split(",");
+        int productID = Integer.parseInt(attributes[0]);
+        String productName = attributes[1];
+        String category = attributes[2];
+        double price = Double.parseDouble(attributes[3]);
+        int quantitySold = Integer.parseInt(attributes[4]);
+
+        return new Product(productID, productName, category, price, quantitySold);
+    }
+
+    public static void main(String[] args) {
+        new Experiment14().run();
+    }
 }
+
+//public class Experiment14 {
+//  class NameStreamBuilder {
+//    public Stream<String> build(){
+//      Stream.Builder<String> builder = Stream.builder(); 
+//      builder.add("Kevan");
+//      builder.add("John");
+//      builder.add("Matthew");
+//      return builder.build();
+//    }
+//  }
+//  
+//  public void run() {
+//    NameStreamBuilder builder = new NameStreamBuilder();
+//    Stream <String> nameStream = builder.build();
+//    nameStream.forEach(n -> System.out.println(n));
+//  }
+//
+//  public static void main(String[] args) {
+//    new Experiment14().run();
+//  }
+//}
