605a1651baf29dec52cf1b57d79308a2ddec14c0
MadMan <thivanka97.bc@gmail.com>
2023-07-03 06:22:55 +0530

    Experimnt 15 & 16

--- functional-programming/src/experiments/Experiment15.java
+++ functional-programming/src/experiments/Experiment15.java
@@ -10,18 +10,62 @@ import java.util.stream.IntStream;
  * 
  * @author Dr Kevan Buckley, University of Wolverhampton, 2019
  ******************************************************************************/
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
 
 public class Experiment15 {
-  public static void main(String[] args) {
-    Function<Integer, Double> reciprocal = new Function<Integer, Double>() {
-      public Double apply(Integer n) {
-        return 1.0 / n;
-      }
-    };
-    System.out.println(reciprocal.apply(10));
+    public static void main(String[] args) {
+        ToDoubleFunction<Product> reciprocalQuantity = new ToDoubleFunction<Product>() {
+            public double applyAsDouble(Product product) {
+                return 1.0 / product.getQuantitySold();
+            }
+        };
 
-    IntStream denominators = IntStream.range(1, 10);
-    denominators.map((IntUnaryOperator) reciprocal)
-        .forEach(x -> System.out.println(x));
-  }
+        String csvFile = "products.csv";
+        Path pathToFile = Paths.get(csvFile);
+
+        try (Stream<String> lines = Files.lines(pathToFile)) {
+            List<Product> products = lines
+                    .skip(1) // Skip the header line
+                    .map(Experiment15::lineToProduct) // Map each line to a Product object
+                    .collect(Collectors.toList()); // Collect results to a List
+
+            System.out.println(reciprocalQuantity.applyAsDouble(products.get(0))); // Apply to the first product
+
+            products.stream().mapToDouble(reciprocalQuantity)
+                .forEach(x -> System.out.println(x));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Product lineToProduct(String line) {
+        String[] attributes = line.split(",");
+        int productID = Integer.parseInt(attributes[0]);
+        String productName = attributes[1];
+        String category = attributes[2];
+        double price = Double.parseDouble(attributes[3]);
+        int quantitySold = Integer.parseInt(attributes[4]);
+
+        return new Product(productID, productName, category, price, quantitySold);
+    }
 }
+
+//public class Experiment15 {
+//  public static void main(String[] args) {
+//    Function<Integer, Double> reciprocal = new Function<Integer, Double>() {
+//      public Double apply(Integer n) {
+//        return 1.0 / n;
+//      }
+//    };
+//    System.out.println(reciprocal.apply(10));
+//
+//    IntStream denominators = IntStream.range(1, 10);
+//    denominators.map((IntUnaryOperator) reciprocal)
+//        .forEach(x -> System.out.println(x));
+//  }
+//}
--- functional-programming/src/experiments/Experiment16.java
+++ functional-programming/src/experiments/Experiment16.java
@@ -10,14 +10,56 @@ import java.util.function.Supplier;
  * @author Dr Kevan Buckley, University of Wolverhampton, 2019
  ******************************************************************************/
 
+import java.nio.file.*;
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
 public class Experiment16 {
-  public static void main(String[] args) {
-    Supplier<String> dateSupplier = new Supplier<String>() {
-      public String get() {
-        LocalDate date = LocalDate.now(); 
-        return date.toString();
-      }
-    };  
-    System.out.printf("Today is %s", dateSupplier.get());
-  }
+    public static void main(String[] args) {
+        Supplier<Product> firstProductSupplier = new Supplier<Product>() {
+            public Product get() {
+                String csvFile = "products.csv";
+                Path pathToFile = Paths.get(csvFile);
+
+                try (Stream<String> lines = Files.lines(pathToFile)) {
+                    return lines
+                            .skip(1) // Skip the header line
+                            .map(Experiment16::lineToProduct) // Map each line to a Product object
+                            .findFirst().orElse(null); // Return the first product or null if the list is empty
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        };
+
+        System.out.printf("The first product is %s", firstProductSupplier.get());
+    }
+
+    private static Product lineToProduct(String line) {
+        String[] attributes = line.split(",");
+        int productID = Integer.parseInt(attributes[0]);
+        String productName = attributes[1];
+        String category = attributes[2];
+        double price = Double.parseDouble(attributes[3]);
+        int quantitySold = Integer.parseInt(attributes[4]);
+
+        return new Product(productID, productName, category, price, quantitySold);
+    }
 }
+
+
+//public class Experiment16 {
+//  public static void main(String[] args) {
+//    Supplier<String> dateSupplier = new Supplier<String>() {
+//      public String get() {
+//        LocalDate date = LocalDate.now(); 
+//        return date.toString();
+//      }
+//    };  
+//    System.out.printf("Today is %s", dateSupplier.get());
+//  }
+//}
