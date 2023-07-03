58a72b979d0ce0a74490f4041aa9edec89c71ae6
MadMan <thivanka97.bc@gmail.com>
2023-07-03 05:34:50 +0530

    GUI lambda expressions added

--- functional-programming/src/gui/GUI01.java
+++ functional-programming/src/gui/GUI01.java
@@ -12,22 +12,48 @@ import javax.swing.*;
  * @author Dr Kevan Buckley, University of Wolverhampton, 2019
  ******************************************************************************/
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUI01 {
 
-  public static void main(String[] args) {
-    JFrame f = new JFrame();
-    JButton b = new JButton("Press Me");
-    f.setLayout(new BorderLayout());
-    f.add(b, BorderLayout.CENTER);
-    
-    b.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent event) {
-        System.out.println("Button says: " + 
-          event.getActionCommand());        
-      }
-    });
-        
-    f.pack();
-    f.setVisible(true);
-  }
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JButton b = new JButton("Press Me");
+        f.setLayout(new BorderLayout());
+        f.add(b, BorderLayout.CENTER);
+
+        b.addActionListener(event -> {
+            String buttonLabel = event.getActionCommand();
+            String uppercaseLabel = buttonLabel.toUpperCase();
+            System.out.println("Button says: " + uppercaseLabel);
+        });
+
+        f.pack();
+        f.setVisible(true);
+    }
 }
+
+
+//public class GUI01 {
+//
+//  public static void main(String[] args) {
+//    JFrame f = new JFrame();
+//    JButton b = new JButton("Press Me");
+//    f.setLayout(new BorderLayout());
+//    f.add(b, BorderLayout.CENTER);
+//    
+//    b.addActionListener(new ActionListener() {
+//      public void actionPerformed(ActionEvent event) {
+//        System.out.println("Button says: " + 
+//          event.getActionCommand());        
+//      }
+//    });
+//        
+//    f.pack();
+//    f.setVisible(true);
+//  }
+//}
--- functional-programming/src/gui/GUI02.java
+++ functional-programming/src/gui/GUI02.java
@@ -10,18 +10,42 @@ import javax.swing.*;
  * @author Dr Kevan Buckley, University of Wolverhampton, 2019
  ******************************************************************************/
 
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
 public class GUI02 {
 
-  public static void main(String[] args) {
-    JFrame f = new JFrame();
-    JButton b = new JButton("Press Me");
-    f.setLayout(new BorderLayout());
-    f.add(b, BorderLayout.CENTER);
-    b.addActionListener(
-        event -> System.out.println("Button says: " + 
-                   event.getActionCommand()));
-    f.pack();
-    f.setVisible(true);
-  }
-
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JButton b = new JButton("Press Me");
+        f.setLayout(new BorderLayout());
+        f.add(b, BorderLayout.CENTER);
+        b.addActionListener(
+            event -> {
+                String buttonLabel = event.getActionCommand();
+                String uppercaseLabel = buttonLabel.toUpperCase();
+                System.out.println("Button says: " + uppercaseLabel);
+            }
+        );
+        f.pack();
+        f.setVisible(true);
+    }
 }
+
+//public class GUI02 {
+//
+//  public static void main(String[] args) {
+//    JFrame f = new JFrame();
+//    JButton b = new JButton("Press Me");
+//    f.setLayout(new BorderLayout());
+//    f.add(b, BorderLayout.CENTER);
+//    b.addActionListener(
+//        event -> System.out.println("Button says: " + 
+//                   event.getActionCommand()));
+//    f.pack();
+//    f.setVisible(true);
+//  }
+//
+//}
